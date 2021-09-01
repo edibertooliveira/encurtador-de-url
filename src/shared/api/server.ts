1fd3f6e9ba7157c7104be42fda1e2bfb5bac7f99
Ediberto Oliveira <edibertooliveira@aol.com>
2021-09-01 19:30:46 -0300

    config deploy

--- src/shared/api/server.ts
+++ src/shared/api/server.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import 'reflect-metadata';
 import server from './app';
 
-server.listen(process.env.APP_PORT, () => {
+server.listen(process.env.PORT, () => {
   console.log(
     `\nServer is running on: ${process.env.APP_API_URL}
     \nDocumentation: ${process.env.APP_API_URL}/docs`,
